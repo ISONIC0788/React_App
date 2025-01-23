8b9a82e6bb46c6e1de5b35d8c27657ffb58228d9
ITUZE Agacyo Ebed Meleck <134501684+ISONIC0788@users.noreply.github.com>
2025-01-23 13:16:19 +0000

    server added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Frist solve proble the write ");
+const express = require("express");
+const app = express();
+app.get('/',(req,res)=>{
+res.send("my frist react App");
+});
+app.listen(800,()=>{
+    console.log("is running on port 800");
+});
