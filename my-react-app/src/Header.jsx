1303efc2c8a5622571f81118f5591fadc58d2e9f
EbediMeleck <ituzeebedi12@gmail.com>
2025-03-27 09:40:01 +0200

    initi messag for react learning

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,25 @@
+
+
+function Header(){
+    return(
+        <div className="container-for-nav">
+           <nav className="link-cont"> 
+            <ul>
+                <li><a href="">home</a></li>
+                <li><a href="">Abaut</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Project</a></li>
+              </ul>
+             
+             </nav>
+             <button className="button">
+                Explore More
+              </button>
+            
+        </div>
+       
+    );
+
+}
+
+export default Header
